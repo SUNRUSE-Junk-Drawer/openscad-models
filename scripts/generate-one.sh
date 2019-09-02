fcd48a8eb44621e22e95273a0641fc338eca62af
jameswilddev <jameswilddev@outlook.com>
2019-09-02 22:24:59 +0100

    Removed ".render".

--- scripts/generate-one.sh
+++ scripts/generate-one.sh
@@ -1,4 +1,4 @@
 scad_path=$1
-base_path=`echo $scad_path | rev | cut -c6- | rev`
+base_path=`echo $scad_path | rev | cut -c13- | rev`
 openscad --hardwarnings --render --o $base_path".png" $scad_path
 openscad --hardwarnings --render --o $base_path".stl" $scad_path

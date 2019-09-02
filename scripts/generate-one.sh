0583c94ec680c4d448cdf54e4bde43c241e38dc5
jameswilddev <jameswilddev@outlook.com>
2019-09-02 21:10:02 +0100

    Disabled parallelism as it seems to not report errors.

--- scripts/generate-one.sh
+++ scripts/generate-one.sh
@@ -1,6 +1,4 @@
 scad_path=$1
 base_path=`echo $scad_path | rev | cut -c6- | rev`
-
-openscad --hardwarnings --render --o $base_path".png" $scad_path &
+openscad --hardwarnings --render --o $base_path".png" $scad_path
 openscad --hardwarnings --render --o $base_path".stl" $scad_path
-wait
